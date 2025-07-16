6bbbef8cc39c89f9eb2fc08d9caf33a9ffc825dc
Equirizon <equirizon@gmail.com>
2025-07-16 17:41:10 +0800

    feat: add about me section

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -20,13 +20,13 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' size='icon' className='bg-accent/0 hover:bg-accent/0'>
+        <Button variant='ghost' size='icon'>
           <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
           <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent className='backdrop-blur-lg bg-popover/70 text-muted-foreground' align='end'>
         <DropdownMenuItem onClick={() => setThemeState('theme-light')}>
           <Sun /> Light
         </DropdownMenuItem>
