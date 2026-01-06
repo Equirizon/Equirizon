1c5768547bfdfcd0b0b2cccc706a99f3cf792065
Equirizon <equirizon@gmail.com>
2026-01-06 20:36:30 +0800

    feat: add logout functionality, enhance authentication UI, and improve admin page layout

--- src/components/Admin/Contact.tsx
+++ src/components/Admin/Contact.tsx
@@ -4,7 +4,9 @@ import { supabase } from '@/utils/supabase/client'
 import { useEffect, useState } from 'react'
 
 export default function Contact() {
-  const [data, setData] = useState<{ data: Array<{ id: number; name: string; email: string; message: string }> | null }>({ data: null })
+  const [data, setData] = useState<{
+    data: Array<{ id: number; name: string; email: string; message: string }> | null
+  }>({ data: null })
   useEffect(() => {
     async function fetchContacts() {
       const { data, error } = await supabase.from('contact').select('*').order('created_at', { ascending: false })
@@ -18,29 +20,27 @@ export default function Contact() {
   }, [])
 
   return (
-    <div className='px-4 py-8'>
-      <div className='mx-auto min-w-2xl max-w-4xl'>
-        <h2 className='mb-8 text-3xl font-bold text-gray-900 text-center'>Inbox</h2>
-        <div className='overflow-hidden rounded-lg bg-white shadow'>
-          <table className='min-w-full divide-y divide-gray-200'>
-            <thead className='bg-theme-color-primary/40'>
-              <tr>
-                <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Name</th>
-                <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Email</th>
-                <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Message</th>
+    <div className='mx-auto w-[min(90%,40rem))] py-8 md:max-w-4xl'>
+      <h2 className='mb-8 text-center text-3xl font-bold text-gray-900'>Inbox</h2>
+      <div className='overflow-x-auto rounded-lg bg-white shadow'>
+        <table className='w-full divide-y divide-gray-200 overflow-auto'>
+          <thead className='bg-theme-color-primary/40'>
+            <tr>
+              <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Name</th>
+              <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Email</th>
+              <th className='px-6 py-3 text-left text-lg font-semibold text-gray-900'>Message</th>
+            </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-200'>
+            {data.data?.map((contact) => (
+              <tr key={contact.id} className='hover:bg-gray-50'>
+                <td className='px-6 py-4 text-sm text-gray-900'>{contact.name}</td>
+                <td className='px-6 py-4 text-sm text-gray-600'>{contact.email}</td>
+                <td className='px-6 py-4 text-sm text-gray-600'>{contact.message}</td>
               </tr>
-            </thead>
-            <tbody className='divide-y divide-gray-200'>
-              {data.data?.map((contact) => (
-                <tr key={contact.id} className='hover:bg-gray-50'>
-                  <td className='px-6 py-4 text-sm text-gray-900'>{contact.name}</td>
-                  <td className='px-6 py-4 text-sm text-gray-600'>{contact.email}</td>
-                  <td className='px-6 py-4 text-sm text-gray-600'>{contact.message}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
