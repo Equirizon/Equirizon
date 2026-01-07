9f0560f0a8cdb8341a38a3a95ea87ef519b783c8
Equirizon <equirizon@gmail.com>
2026-01-07 22:21:43 +0800

    fix: skeleton config for debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare'
 const isBuilding = process.env.NODE_ENV === 'production' || process.argv.includes('build')
 
 export default defineConfig({
-  integrations: [react()],
+  // integrations: [react()],
 
   adapter: cloudflare({
     platformProxy: { enabled: true },
@@ -19,28 +19,29 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
-      alias: isBuilding
-        ? {
-            'react-dom/server': 'react-dom/server.edge',
-          }
-        : {},
-      },
-      ssr: {
-        external: ['node:crypto', 'node:condition-hooks', 'node:worker_threads'],
-      },
-  },
-
-  env: {
-    schema: {
-      SUPABASE_URL: envField.string({ context: 'client', access: 'public' }),
-      SUPABASE_ANON_KEY: envField.string({ context: 'client', access: 'public' }),
-      TOTP_SECRET: envField.string({ context: 'server', access: 'secret' }),
-      APP_MODE: envField.enum({
-        values: ['development', 'preview', 'production'],
-        context: 'client',
-        access: 'public',
-        default: 'production',
-      }),
+      // alias: isBuilding
+      //   ? {
+      //       'react-dom/server': 'react-dom/server.edge',
+      //       'crypto': 'node:crypto',
+      //     }
+      //   : {},
+    },
+    ssr: {
+      external: ['node:crypto', 'crypto ', 'node:condition-hooks', 'node:worker_threads'],
     },
   },
+
+  // env: {
+  //   schema: {
+  //     SUPABASE_URL: envField.string({ context: 'client', access: 'public' }),
+  //     SUPABASE_ANON_KEY: envField.string({ context: 'client', access: 'public' }),
+  //     TOTP_SECRET: envField.string({ context: 'server', access: 'secret' }),
+  //     APP_MODE: envField.enum({
+  //       values: ['development', 'preview', 'production'],
+  //       context: 'client',
+  //       access: 'public',
+  //       default: 'production',
+  //     }),
+  //   },
+  // },
 })
