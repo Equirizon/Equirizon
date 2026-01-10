8136d492eb3aa104fc7669513cd34e4b98a5c131
Equirizon <equirizon@gmail.com>
2026-01-10 23:07:38 +0800

    feat: implement rate limiting for OTP verification #33

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -9,6 +9,7 @@ import { cn } from '@/lib/utils.js'
 export default function Auth() {
   const [isInvalid, setIsInvalid] = useState(false)
   const [length, setLength] = useState(0)
+  const [limited, setLimited] = useState(false)
   return (
     <main className='flex h-screen w-screen flex-1 flex-col items-center justify-center p-4'>
       <form
@@ -18,8 +19,13 @@ export default function Auth() {
           const formData = new FormData(form)
           const { error } = await actions.checkOTP(formData)
           if (error) {
-            console.error('Invalid Code')
+            console.error(error)
             setIsInvalid(true)
+            if (error.message === 'Rate limited.') {
+              setLimited(true)
+              setTimeout(() => setLimited(false), 1000 * 300)
+              alert('Too many attempts. Please wait a moment before trying again.')
+            }
           } else {
             window.location.href = '/admin'
             setIsInvalid(false)
@@ -36,10 +42,14 @@ export default function Auth() {
             if (isInvalid && e.target.value.length < 6) {
               setIsInvalid(false)
             }
-            setIsInvalid(false)}}
-          className={cn('bg-white', isInvalid && 'animate-in text-red-500 duration-300')}
+            setIsInvalid(false)
+          }}
+          className={cn('bg-white', isInvalid && 'animate-in text-orange-500 duration-300', limited && 'animate-in text-red-600 duration-300')}
+          disabled={limited}
         />
-        <Button type='submit' disabled={length < 6 || isInvalid}>Unlock</Button>
+        <Button type='submit' disabled={length < 6 || isInvalid || limited}>
+          Unlock
+        </Button>
       </form>
     </main>
   )
